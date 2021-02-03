b3ccc7592956d052fb5c0aa535d9e31e1f154d0d
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-04 00:08:53 +0800

    Update TodoAdapter.kt

--- app/src/main/java/com/thepyprogrammer/prioritytodo/model/TodoAdapter.kt
+++ app/src/main/java/com/thepyprogrammer/prioritytodo/model/TodoAdapter.kt
@@ -1,8 +1,8 @@
 package com.thepyprogrammer.prioritytodo.model
 
 import android.app.DatePickerDialog
-import android.content.Context
 import android.graphics.Paint.STRIKE_THRU_TEXT_FLAG
+import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,15 +10,20 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.thepyprogrammer.prioritytodo.R
+import com.thepyprogrammer.prioritytodo.ui.MainActivity
 import kotlinx.android.synthetic.main.item_todo.view.*
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeFormatterBuilder
 
+
 class TodoAdapter(
+    private val activity: MainActivity,
     val todos: MutableList<Todo> = mutableListOf()
 ) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
-    private val dTF: DateTimeFormatter = DateTimeFormatterBuilder().parseCaseInsensitive().appendPattern("dd/MM/yyyy").toFormatter()
+    private val dTF: DateTimeFormatter = DateTimeFormatterBuilder().parseCaseInsensitive().appendPattern(
+        "dd/MM/yyyy"
+    ).toFormatter()
 
     class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 
@@ -38,11 +43,15 @@ class TodoAdapter(
         todos.sortWith { p1, p2 ->
             when {
                 p1.dueDate > p2.dueDate -> 1
-                p1.dueDate == p2.dueDate -> if(p1.priority > p2.priority) 1 else if(p1.priority < p2.priority) -1 else p1.title.compareTo(p2.title)
+                p1.dueDate == p2.dueDate -> if(p1.priority > p2.priority) 1 else if(p1.priority < p2.priority) -1 else p1.title.compareTo(
+                    p2.title
+                )
                 else -> -1
             }
         }
         notifyDataSetChanged()
+        activity.updateFile()
+
     }
 
     private fun toggleStrikeThrough(tvTodoTitle: TextView, isChecked: Boolean, dateView: Button) {
@@ -71,12 +80,26 @@ class TodoAdapter(
             dateView.setOnClickListener {
                 val datePickerDialog = DatePickerDialog(context,
                     { _, year, monthOfYear, dayOfMonth ->
-                        curTodo.dueDate = LocalDate.of(year, monthOfYear+1, dayOfMonth)
+                        curTodo.dueDate = LocalDate.of(year, monthOfYear + 1, dayOfMonth)
                         notifyItemChanged(position)
-                    }, curTodo.dueDate.year, curTodo.dueDate.monthValue - 1, curTodo.dueDate.dayOfMonth)
+                    },
+                    curTodo.dueDate.year,
+                    curTodo.dueDate.monthValue - 1,
+                    curTodo.dueDate.dayOfMonth
+                )
                 datePickerDialog.datePicker.minDate = System.currentTimeMillis() - 1000;
                 datePickerDialog.show()
             }
+
+            todoTitleView.setOnKeyListener(object : View.OnKeyListener {
+                override fun onKey(v: View?, keyCode: Int, event: KeyEvent): Boolean {
+                    if (keyCode == KeyEvent.KEYCODE_ENTER && todoTitleView.text.toString().isNotEmpty()) {
+                        curTodo.title = todoTitleView.text.toString()
+                        return true
+                    }
+                    return false
+                }
+            })
         }
     }
 
