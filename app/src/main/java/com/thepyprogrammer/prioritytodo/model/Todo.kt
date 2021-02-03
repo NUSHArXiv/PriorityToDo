adda4d7e223aaff6e1e992442ebda4407c3bf3bc
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-04 00:24:43 +0800

    Reformat Todo.kt

--- app/src/main/java/com/thepyprogrammer/prioritytodo/model/Todo.kt
+++ app/src/main/java/com/thepyprogrammer/prioritytodo/model/Todo.kt
@@ -4,12 +4,12 @@ import com.thepyprogrammer.prioritytodo.ui.MainActivity
 import java.time.LocalDate
 
 data class Todo(
-    var title: String,
-    var priority: Float,
-    var dueDate: LocalDate,
-    var isChecked: Boolean = false
+        var title: String,
+        var priority: Float,
+        var dueDate: LocalDate,
+        var isChecked: Boolean = false
 ) {
     override fun toString(): String {
-        return "$title $priority "+MainActivity.dTF.format(dueDate)+" $isChecked"
+        return "$title $priority " + MainActivity.dTF.format(dueDate) + " $isChecked"
     }
 }
