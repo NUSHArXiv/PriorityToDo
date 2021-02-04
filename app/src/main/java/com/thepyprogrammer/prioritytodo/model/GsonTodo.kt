b8dc2eb07a9ceaf31843389918d45800a2a2bcb8
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-04 22:32:13 +0800

    Create POJO GsonTodo.kt

---
+++ app/src/main/java/com/thepyprogrammer/prioritytodo/model/GsonTodo.kt
@@ -0,0 +1,19 @@
+package com.thepyprogrammer.prioritytodo.model
+
+import com.google.gson.annotations.SerializedName
+import com.thepyprogrammer.prioritytodo.ui.MainActivity
+import java.time.LocalDate
+
+data class GsonTodo(
+    @SerializedName("title") var title: String,
+    @SerializedName("priority") var priority: String,
+    @SerializedName("dueDate") var dueDate: String,
+    @SerializedName("isChecked") var isChecked: String,
+    @SerializedName("description") var description: String = ""
+) {
+    constructor(todo: Todo): this(todo.title, "${todo.priority}", MainActivity.dTF.format(todo.dueDate), "${todo.isChecked}", todo.description)
+
+    fun toTodo(): Todo {
+        return Todo(title, priority.toFloat(), LocalDate.parse(dueDate, MainActivity.dTF), isChecked.toBoolean(), description)
+    }
+}
