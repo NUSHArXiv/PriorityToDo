d06bc772f44f046f9072e205e9bd49774b8e74f6
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-02-04 19:45:15 +0800

    Implement I/O in MainActivity.kt

--- app/src/main/java/com/thepyprogrammer/prioritytodo/ui/MainActivity.kt
+++ app/src/main/java/com/thepyprogrammer/prioritytodo/ui/MainActivity.kt
@@ -2,18 +2,23 @@ package com.thepyprogrammer.prioritytodo.ui
 
 import android.app.DatePickerDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.thepyprogrammer.prioritytodo.R
+import com.thepyprogrammer.prioritytodo.model.SwipeToDeleteCallback
 import com.thepyprogrammer.prioritytodo.model.Todo
 import com.thepyprogrammer.prioritytodo.model.TodoAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 import java.io.File
+import java.io.IOException
 import java.io.PrintWriter
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeFormatterBuilder
 import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,6 +36,8 @@ class MainActivity : AppCompatActivity() {
 
         rvTodoItems.adapter = todoAdapter
         rvTodoItems.layoutManager = LinearLayoutManager(this)
+        val itemTouchHelper = ItemTouchHelper(SwipeToDeleteCallback(todoAdapter))
+        itemTouchHelper.attachToRecyclerView(rvTodoItems)
 
         priorityBar.rating = 2.5F
 
@@ -57,26 +64,35 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun readFile(): MutableList<Todo> {
+        try {
         val dbFile = File(filesDir.path.toString() + "/todos.txt")
         if (!dbFile.exists()) dbFile.createNewFile()
         val sc = Scanner(dbFile)
         val list = mutableListOf<Todo>()
         while (sc.hasNext()) {
-            val s = sc.nextLine().split(" ")
-            list.add(Todo(s[0], s[1].toFloat(), dTF.parse(s[2]) as LocalDate, s[3].toBoolean()))
+            val s = sc.nextLine().split("\t")
+            list.add(Todo(s[0], s[1].toFloat(), LocalDate.parse(s[2], dTF) as LocalDate, s[3].toBoolean()))
         }
         sc.close()
         return list
+        } catch (ex: IOException) {
+            Log.i("ERR", ex.stackTrace.toString())
+            return mutableListOf()
+        }
     }
 
     fun updateFile() {
-        val dbFile = File(filesDir.path.toString() + "/todos.txt")
-        if (!dbFile.exists()) dbFile.createNewFile()
-        val pw = PrintWriter(dbFile)
-        todoAdapter.todos.forEach {
-            pw.println(it)
+        try {
+            val dbFile = File(filesDir.path.toString() + "/todos.txt")
+            if (!dbFile.exists()) dbFile.createNewFile()
+            val pw = PrintWriter(dbFile)
+            todoAdapter.todos.forEach {
+                pw.println(it)
+            }
+            pw.close()
+        } catch (ex: IOException) {
+            Log.i("ERR", ex.stackTrace.toString())
         }
-        pw.close()
     }
 
     override fun onDestroy() {
