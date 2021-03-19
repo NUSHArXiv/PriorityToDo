5242d0c31ce7df948d0dc30491bf3ef8de6416d1
ThePyProgrammer <50899509+ThePyProgrammer@users.noreply.github.com>
2021-03-19 17:00:37 +0800

    Update Todo.kt

--- app/src/main/java/com/thepyprogrammer/prioritytodo/model/Todo.kt
+++ app/src/main/java/com/thepyprogrammer/prioritytodo/model/Todo.kt
@@ -1,6 +1,5 @@
 package com.thepyprogrammer.prioritytodo.model
 
-import com.google.gson.annotations.SerializedName
 import com.thepyprogrammer.prioritytodo.ui.MainActivity
 import java.time.LocalDate
 
@@ -12,7 +11,7 @@ data class Todo(
     var description: String = ""
 ) {
     override fun toString(): String {
-        val dueDateString = MainActivity.dTF.format(dueDate);
+        val dueDateString = MainActivity.dTF.format(dueDate)
         return "$title\t$priority\t$dueDateString\t$isChecked"
     }
 }
